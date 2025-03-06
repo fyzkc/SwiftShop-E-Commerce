413ddab79ff3cef1e43d07bbc1d880e0e65579e0
Feyza <koc.feyza.fb@gmail.com>
2025-03-06 16:02:23 +0300

    Settings folder created for Catalog microservice.

---
+++ Services/Catalog/SwiftShop.Catalog/Settings/DatabaseSettings.cs
@@ -0,0 +1,30 @@
+﻿namespace SwiftShop.Catalog.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string CategoryCollectionName 
+        { 
+            get => throw new NotImplementedException(); set => throw new NotImplementedException(); 
+        }
+        public string ProductCollectionName
+        { 
+            get => throw new NotImplementedException(); set => throw new NotImplementedException(); 
+        }
+        public string ProductDetailCollectionName 
+        { 
+            get => throw new NotImplementedException(); set => throw new NotImplementedException(); 
+        }
+        public string ProductImageCollectionName 
+        { 
+            get => throw new NotImplementedException(); set => throw new NotImplementedException(); 
+        }
+        public string ConnectionString 
+        { 
+            get => throw new NotImplementedException(); set => throw new NotImplementedException(); 
+        }
+        public string DatabaseName 
+        { 
+            get => throw new NotImplementedException(); set => throw new NotImplementedException(); 
+        }
+    }
+}
