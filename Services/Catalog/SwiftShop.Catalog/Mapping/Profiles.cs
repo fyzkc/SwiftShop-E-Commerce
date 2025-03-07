85a8590a0ec35af6216fa86d565bd564bb18e259
Feyza <koc.feyza.fb@gmail.com>
2025-03-07 03:36:16 +0300

    Automapper configurations done.

---
+++ Services/Catalog/SwiftShop.Catalog/Mapping/Profiles.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using SwiftShop.Catalog.Dtos.CategoryDtos;
+using SwiftShop.Catalog.Dtos.ProductDetailDtos;
+using SwiftShop.Catalog.Dtos.ProductDtos;
+using SwiftShop.Catalog.Dtos.ProductImageDtos;
+using SwiftShop.Catalog.Entities;
+
+namespace SwiftShop.Catalog.Mapping
+{
+    public class Profiles : Profile
+    {
+        public Profiles()
+        {
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, ResultCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+
+            CreateMap<ProductDetail, CreateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, ResultProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, UpdateProductDetailDto>().ReverseMap();            
+
+            CreateMap<ProductImage, CreateProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, ResultProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, UpdateProductImageDto>().ReverseMap();
+        }
+    }
+}
