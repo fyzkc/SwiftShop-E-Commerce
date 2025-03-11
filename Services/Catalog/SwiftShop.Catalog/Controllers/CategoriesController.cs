d5c86e934a1072a5de449e955643b680ff70e9f2
Feyza <koc.feyza.fb@gmail.com>
2025-03-11 22:21:46 +0300

    CategoriesController created.

---
+++ Services/Catalog/SwiftShop.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwiftShop.Catalog.Dtos.CategoryDtos;
+using SwiftShop.Catalog.Services.CategoryServices;
+
+namespace SwiftShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var allCategories = await _categoryService.GetAllCategoriesAsync();
+            return Ok(allCategories);
+        }
+
+        [HttpGet("{categoryId}")]
+        public async Task<IActionResult> GetCategoryById(string categoryId)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(categoryId);
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            await _categoryService.CreateCategoryAsync(createCategoryDto);
+            return Ok("Category added succesfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            await _categoryService.UpdateCategoryAsync(updateCategoryDto);
+            return Ok("Category updated succesfully");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(string categoryId)
+        {
+            await _categoryService.DeleteCategoryAsync(categoryId);
+            return Ok("Category deleted succesfully");
+        }
+    }
+}
