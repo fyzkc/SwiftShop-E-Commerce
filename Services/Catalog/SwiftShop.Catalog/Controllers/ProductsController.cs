befd7b3f7f987e06ec3fe53ab925db4c69528a5b
Feyza <koc.feyza.fb@gmail.com>
2025-03-11 22:27:22 +0300

    ProductsController created.

---
+++ Services/Catalog/SwiftShop.Catalog/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwiftShop.Catalog.Dtos.CategoryDtos;
+using SwiftShop.Catalog.Dtos.ProductDtos;
+using SwiftShop.Catalog.Services.CategoryServices;
+using SwiftShop.Catalog.Services.ProductServices;
+
+namespace SwiftShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var allProducts = await _productService.GetAllProductsAsync();
+            return Ok(allProducts);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<IActionResult> GetProductById(string productId)
+        {
+            var product = await _productService.GetProductByIdAsync(productId);
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            await _productService.CreateProductAsync(createProductDto);
+            return Ok("Product added succesfully");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            await _productService.UpdateProductAsync(updateProductDto);
+            return Ok("Product updated succesfully");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct(string productId)
+        {
+            await _productService.DeleteProductAsync(productId);
+            return Ok("Product deleted succesfully");
+        }
+    }
+}
