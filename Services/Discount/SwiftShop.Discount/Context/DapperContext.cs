4f9239521f5e706c0db35bd20bfad4b898b0291a
Feyza <koc.feyza.fb@gmail.com>
2025-03-12 22:28:49 +0300

    DapperContext class and first migration for Coupon entity are created.

---
+++ Services/Discount/SwiftShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SwiftShop.Discount.Entities;
+using System.Data;
+
+namespace SwiftShop.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-ES8AIES\\SQLEXPRESS;Initial Catalog=SwiftShopDiscountDb;TrustServerCertificate=True; Integrated Security=True;");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
