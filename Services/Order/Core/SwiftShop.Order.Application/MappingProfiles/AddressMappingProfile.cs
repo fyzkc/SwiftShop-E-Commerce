26e1e7b44dd15fe4ebba4442f6ff3a970ad7c529
Feyza <koc.feyza.fb@gmail.com>
2025-03-22 19:21:27 +0300

    refactored.

--- Services/Order/Core/SwiftShop.Order.Application/MappingProfiles/AddressMappingProfile.cs
+++ Services/Order/Core/SwiftShop.Order.Application/MappingProfiles/AddressMappingProfile.cs
@@ -14,10 +14,10 @@ namespace SwiftShop.Order.Application.MappingProfiles
     {
         public AddressMappingProfile()
         {
-            CreateMap<CreateAddressCommand, Address>(); //we are mapping the CreateAddressCommand class to the Address entity for creation processes. 
-            CreateMap<UpdateAddressCommand, Address>(); //we are mapping the UpdateAddressCommand class to the Address entity for updating processes. 
-            CreateMap<Address, GetAddressQueryResult>(); //we are mapping the Address entity to the GetAddressQueryResult class for resulting processes. 
-            CreateMap<Address, GetAddressByIdQueryResult>(); //we are mapping the Address entity to the GetAddressByIdQueryResult class for resulting processes. 
+            CreateMap<CreateAddressCommand, Address>(); //we are turning the CreateAddressCommand class into the Address entity for creation processes. 
+            CreateMap<UpdateAddressCommand, Address>(); //we are turning the UpdateAddressCommand class into the Address entity for updating processes. 
+            CreateMap<Address, GetAddressQueryResult>(); //we are turning the Address entity into the GetAddressQueryResult class for resulting processes. 
+            CreateMap<Address, GetAddressByIdQueryResult>(); //we are turning the Address entity into the GetAddressByIdQueryResult class for resulting processes. 
         }
     }
 }
