f7e7f9503803189f915fa25ee0143b7ca7a38fa1
Feyza <koc.feyza.fb@gmail.com>
2025-03-22 18:49:56 +0300

    ServiceRegistration class is created for all the services that will be used in later. And for now, only added the AutoMapper.

---
+++ Services/Order/Core/SwiftShop.Order.Application/Configurations/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftShop.Order.Application.Configurations
+{
+    public static class ServiceRegistration // this class is created for the services that will be using in the future. 
+    //we can do it in Program.cs too, but in the future if we have more than one service, this class makes it more clear to use.
+    //otherwise we should add all the services one by one to the Program.cs. But with this class, we can add our services into here and only add the 
+    //AddApplicationServices method into the Program.cs.
+    {
+        public static void AddApplicationServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
