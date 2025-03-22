5827cf2c8d97f6dee321683a5d5a72ecd17c9051
Feyza <koc.feyza.fb@gmail.com>
2025-03-22 18:52:34 +0300

    Service configuration method from the ServiceRegistration class is added to the Program.cs

--- Services/Order/Presentation/SwiftShop.Order.API/Program.cs
+++ Services/Order/Presentation/SwiftShop.Order.API/Program.cs
@@ -1,3 +1,5 @@
+using SwiftShop.Order.Application.Configurations; // we took a reference from the Application layer.
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationServices(); //we add the AddApplicationServices method from the ServiceRegistration class. 
 
 var app = builder.Build();
 
