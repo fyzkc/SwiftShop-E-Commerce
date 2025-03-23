a6278c22a651ff824a6921a1119ccd8bd5123a09
Feyza <koc.feyza.fb@gmail.com>
2025-03-23 16:02:50 +0300

    Delete and Update handlers created.

---
+++ Services/Order/Core/SwiftShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/RemoveAddressCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using SwiftShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using SwiftShop.Order.Application.Interfaces;
+using SwiftShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class RemoveAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+        private readonly IMapper _mapper;
+
+        public RemoveAddressCommandHandler(IRepository<Address> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task Handle(RemoveAddressCommand removeAddressCommand)
+        {
+            var removingValue = await _repository.GetByIdAsync(removeAddressCommand.AddressId);
+            await _repository.DeleteAsync(removingValue);
+        }
+    }
+}
---
+++ Services/Order/Core/SwiftShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/UpdateAddressCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using SwiftShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using SwiftShop.Order.Application.Interfaces;
+using SwiftShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class UpdateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+        private readonly IMapper _mapper;
+
+        public UpdateAddressCommandHandler(IRepository<Address> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task Handle(UpdateAddressCommand updateAddressCommand)
+        {
+            var address = _mapper.Map<Address>(updateAddressCommand);
+            await _repository.UpdateAsync(address);
+        }
+    }
+}
