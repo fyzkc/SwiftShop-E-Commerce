5b629bb4eebb70916643d2673fc4b924d55845a4
Feyza <koc.feyza.fb@gmail.com>
2025-03-23 15:32:20 +0300

    GetAll handler method is created.

---
+++ Services/Order/Core/SwiftShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using SwiftShop.Order.Application.Features.CQRS.Results.AddressResults;
+using SwiftShop.Order.Application.Interfaces;
+using SwiftShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class GetAddressQueryHandler
+    {
+        private readonly IRepository<Address> _repository;
+        private readonly IMapper _mapper;
+
+        public GetAddressQueryHandler(IRepository<Address> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetAddressQueryResult>> Handle()
+        {
+            var addresses = await _repository.GetAllAsync();
+            return _mapper.Map<List<GetAddressQueryResult>>(addresses);
+        }
+    }
+}
