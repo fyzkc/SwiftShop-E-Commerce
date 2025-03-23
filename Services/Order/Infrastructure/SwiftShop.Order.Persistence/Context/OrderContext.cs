05acdd1e9e35dc3379b35cad5ef0962a2fd49d9d
Feyza <koc.feyza.fb@gmail.com>
2025-03-23 16:19:13 +0300

    Context class is created.

---
+++ Services/Order/Infrastructure/SwiftShop.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SwiftShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftShop.Order.Persistence.Context
+{
+    public class OrderContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=...; Initial Catalog=SwiftShopOrderDb; Integrated Security = True; TrustServerCertificate=True;");
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Ordering> Orderings { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
