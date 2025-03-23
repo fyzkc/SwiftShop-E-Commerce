dc470aee0b22c0c5b5f9333890a4023d7d629e14
Feyza <koc.feyza.fb@gmail.com>
2025-03-23 22:47:46 +0300

    refactored.

--- Services/Order/Core/SwiftShop.Order.Application/Interfaces/IRepository.cs
+++ Services/Order/Core/SwiftShop.Order.Application/Interfaces/IRepository.cs
@@ -11,9 +11,9 @@ namespace SwiftShop.Order.Application.Interfaces
     {
         Task<List<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
-        Task<T> CreateAsync(T entity);
-        Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(T entity); //we should give the entity to the Delete method instead of giving the id.
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity); //we should give the entity to the Delete method instead of giving the id.
         //because, delete method should only do the deletion process. If we give an Id parameter to this method, first it will try to find the data from the database,
         //then delete it. And according to SOLID principles this approach is not useful. A method must have only one job. 
         Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter); //this line is using for filtering with Entity Framework.
