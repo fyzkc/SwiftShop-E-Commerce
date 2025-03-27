2970c5f1626f46e3b4fc1daa0462eb7d7b08cc8d
Feyza <koc.feyza.fb@gmail.com>
2025-03-27 15:56:52 +0300

    InfrastructureServiceRegistration class created for IRepository and Repository service building. And added to the Program.cs file

---
+++ Services/Order/Infrastructure/SwiftShop.Order.Persistence/InfrastructureConfigurations/InfrastructureServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SwiftShop.Order.Application.Interfaces;
+using SwiftShop.Order.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftShop.Order.Persistence.InfrastructureConfigurations
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
--- Services/Order/Presentation/SwiftShop.Order.API/Program.cs
+++ Services/Order/Presentation/SwiftShop.Order.API/Program.cs
@@ -1,8 +1,25 @@
-using SwiftShop.Order.Application.Configurations; // we took a reference from the Application layer.
+using SwiftShop.Order.Application.Configurations;
+using SwiftShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using SwiftShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using SwiftShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using SwiftShop.Order.Persistence.InfrastructureConfigurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddScoped<CreateAddressCommandHandler>();
+builder.Services.AddScoped<UpdateAddressCommandHandler>();
+builder.Services.AddScoped<RemoveAddressCommandHandler>();
+
+builder.Services.AddScoped<GetOrderDetailByIdQuery>();
+builder.Services.AddScoped<GetOrderDetailQueryHandler>();
+builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
+builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,6 +27,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplicationServices(); //we add the AddApplicationServices method from the ServiceRegistration class. 
+builder.Services.AddInfrastructureServices();
 
 var app = builder.Build();
 
