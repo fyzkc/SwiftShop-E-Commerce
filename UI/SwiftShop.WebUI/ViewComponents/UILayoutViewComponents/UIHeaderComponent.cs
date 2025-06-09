5cd36ac5d82369f3ed5ef20b07ddafc101c550df
Feyza <koc.feyza.fb@gmail.com>
2025-06-09 23:54:16 +0300

    Header and Topbar components are created for UI panel.

---
+++ UI/SwiftShop.WebUI/ViewComponents/UILayoutViewComponents/UIHeaderComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwiftShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    //[ViewComponent(Name = "UIHeader")]
+    public class UIHeaderComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ UI/SwiftShop.WebUI/ViewComponents/UILayoutViewComponents/UITopbarComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwiftShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    //[ViewComponent(Name = "UITopbar")]
+    public class UITopbarComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
