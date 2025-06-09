88cbd001ddfaeb781c76f995bc412b609bdab208
Feyza <koc.feyza.fb@gmail.com>
2025-06-09 16:27:39 +0300

    Authorization policies are defined and the other services are added.

--- Services/Cart/SwiftShop.Cart/Program.cs
+++ Services/Cart/SwiftShop.Cart/Program.cs
@@ -1,4 +1,10 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using SwiftShop.Cart.Services;
+using SwiftShop.Cart.Settings;
+using SwiftShop.Cart.SharedIdentity;
+using System.IdentityModel.Tokens.Jwt;
+
 namespace SwiftShop.Cart
 {
     public class Program
@@ -7,12 +13,55 @@ namespace SwiftShop.Cart
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub"); //for removing the mapping of sub value.
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.Authority = builder.Configuration["IdentityServerUrl"];
+                opt.Audience = "CartResource";
+                opt.RequireHttpsMetadata = false;
+            });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                //its necessary to have CatalogReadPermission or CatalogFullPermission to make a GET request.
+                options.AddPolicy("CartReadOrFullPolicy", policy =>
+                    policy.RequireAssertion(context =>
+                        context.User.HasClaim(c =>
+                            (c.Type == "scope" && c.Value == "CartReadPermission") ||
+                            (c.Type == "scope" && c.Value == "CartFullPermission")
+                        )
+                    )
+                );
+
+                //its necessary to have CatalogFullPermission to make a POST request. 
+                options.AddPolicy("CartFullPolicy", policy =>
+                    policy.RequireClaim("scope", "CartFullPermission")
+                );
+            });
+
             // Add services to the container.
+
+            builder.Services.AddSingleton<RedisService>(sp =>
+            {
+                var config = sp.GetRequiredService<IConfiguration>();
+                var redisSettings = config.GetSection("RedisSettings").Get<RedisSettings>();
+
+                var redisService = new RedisService(redisSettings.Host, redisSettings.Port);
+                redisService.Connect();
+                return redisService;
+            });
+
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+
+            builder.Services.AddScoped<ICartService, CartService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
 
             var app = builder.Build();
 
@@ -24,6 +73,8 @@ namespace SwiftShop.Cart
             }
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
