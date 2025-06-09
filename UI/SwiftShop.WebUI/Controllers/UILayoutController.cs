dc77c388b19bae6b2b13152f8b92cf16907c74e6
Feyza <koc.feyza.fb@gmail.com>
2025-06-09 18:31:18 +0300

    UI service is created.

---
+++ UI/SwiftShop.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwiftShop.WebUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult _UILayout()
+        {
+            return View();
+        }
+    }
+}
