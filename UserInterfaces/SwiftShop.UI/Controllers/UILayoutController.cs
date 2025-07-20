5fda93b2fb55f79502ae8e30a4c400c60eeeaf85
Feyza <koc.feyza.fb@gmail.com>
2025-07-20 21:11:23 +0300

    Index page of the template has added to the UILayout View page and Home View page.

--- UserInterfaces/SwiftShop.UI/Controllers/UILayoutController.cs
+++ UserInterfaces/SwiftShop.UI/Controllers/UILayoutController.cs
@@ -2,7 +2,7 @@
 
 namespace SwiftShop.UI.Controllers
 {
-    public class HomeController : Controller
+    public class UILayoutController : Controller
     {
         public IActionResult Index()
         {
