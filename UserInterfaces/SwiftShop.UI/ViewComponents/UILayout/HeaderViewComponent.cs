f615ba225828e625195525000d9241a24a82ceb4
Feyza <koc.feyza.fb@gmail.com>
2025-08-11 19:29:05 +0300

    HeaderViewComponent created and implemented.

---
+++ UserInterfaces/SwiftShop.UI/ViewComponents/UILayout/HeaderViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SwiftShop.UI.ViewComponents.UILayout
+{
+    [ViewComponent(Name = "HeaderViewComponent")]
+    public class HeaderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
